5f6cd80909fc01688e6416e41d9f69b8b5600abc
Morgan Roderick <morgan@roderick.dk>
2026-01-25 10:52:55 +0100

    refactor: remove MemoryIdempotencyStore from public API

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export type {
 } from "./types.js";
 
 // Store implementations
-export { MemoryIdempotencyStore } from "./store/memory.js";
 export { SqliteIdempotencyStore } from "./store/sqlite.js";
 
 // Utilities
