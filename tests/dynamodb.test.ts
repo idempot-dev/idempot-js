1b8cb35169f8871460f0f389716cedf6d6e12f38
Morgan Roderick <morgan@roderick.dk>
2026-01-25 13:51:27 +0100

    test: add DynamoDB lookup test for empty store

--- tests/dynamodb.test.ts
+++ tests/dynamodb.test.ts
@@ -1,6 +1,6 @@
 import { test } from "tap";
 import { mockClient } from "aws-sdk-client-mock";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDbIdempotencyStore } from "../src/store/dynamodb.js";
 
 test("DynamoDbIdempotencyStore - initialization", (t) => {
@@ -24,4 +24,19 @@ test("DynamoDbIdempotencyStore - initialization with custom table name", (t) => 
 
   t.ok(store, "store should be created with custom table");
   t.end();
+});
+
+test("DynamoDbIdempotencyStore - lookup with empty store", async (t) => {
+  const ddbMock = mockClient(DynamoDBDocumentClient);
+  ddbMock.on(GetCommand).resolves({});
+  ddbMock.on(QueryCommand).resolves({ Items: [] });
+
+  const store = new DynamoDbIdempotencyStore({
+    client: ddbMock as any
+  });
+
+  const result = await store.lookup("test-key", "test-fingerprint");
+
+  t.equal(result.byKey, null, "byKey should be null");
+  t.equal(result.byFingerprint, null, "byFingerprint should be null");
 });
